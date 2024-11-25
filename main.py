4bb32b388b1ea13ade47571d44b0f6092b94ad33
Bhavish1922 <tumalapentabhavish@gmail.com>
2024-11-25 12:02:43 +0530

    v_1.1

--- main.py
+++ main.py
@@ -10,45 +10,35 @@ ALPHABET= [
         ',', '-', '.', '/', ':',';','<','=','>','?',
         '@','[','\\',']', '^','_','`','{','|','}','~'
     ]
+
+def secret(text, shift_amount, direction):
+    """_Takes string as an input and do either encode or decode and gives the result_
+
+    Args:
+        text (_string_)
+        shift_amount (_int_)
+        direction (_string_)
+    """
+    output_text= ""
+    if direction == "Decode":
+        shift_amount = -shift_amount
+    for letter in text:
+        if letter in ALPHABET:
+            shifted = ALPHABET.index(letter) + shift_amount
+            shifted %= len(ALPHABET)
+            output_text += ALPHABET[shifted]
+        else:
+            output_text += letter
+    print(f"{direction}d message: {output_text}")
+
 EXIT= False
 while not EXIT:
     original_text= input("Enter the text: ").upper()
     shifts= int(input("No.of shifts: "))
-    state= input("Enter (Encode or Decode): ")
-
-    def encode(text, shift_amount):
-        """_This function takes the text and shift_amount as inputs and
-            encode in ceaser cipher algorithm_
-
-        Args:
-            text (_string_): _original text_
-            shift_amount (_integer_): _number of shifts_
-        """
-        encoded_text= ""
-        for letter in text:
-            shifted = ALPHABET.index(letter) + shift_amount
-            shifted %= len(ALPHABET)
-            encoded_text += ALPHABET[shifted]
-        print(f"Encoded message: {encoded_text}")
-
-    def decode(text, shift_amount):
-        """_This function takes the text and shift_amount as inputs and
-            decode in ceaser cipher algorithm_
-
-        Args:
-            text (_string_): _original text_
-            shift_amount (_integer_): _number of shifts_
-        """
-        output_text= ""
-        for letter in text:
-            shifted = ALPHABET.index(letter) - shift_amount
-            shifted %= len(ALPHABET)
-            output_text += ALPHABET[shifted]
-        print(f"Decoded message: {output_text}")
-    if state == "Encode":
-        encode(text=original_text, shift_amount=shifts)
-    elif state == "Decode":
-        decode(text=original_text, shift_amount=shifts)
+    encode_decode= input("Encode (or) Decode: ")
+    
+    secret(text=original_text, shift_amount=shifts, direction=encode_decode)
     CONTINUE= input("Do want to CONTINUE (yes or no): ")
+
     if CONTINUE == "no":
         EXIT= True
